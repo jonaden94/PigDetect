580770e9242afa932c4709d7bfb26531029cd657
GOESTERN-0883963 <u12041@glogin9.usr.hlrn.de>
2024-07-30 18:02:38 +0200

    update setup file to support newer python and pytorch version

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,27 +1,17 @@
 ############## 1) create environment with torch
 ENV_NAME='detection'
 conda env remove -n $ENV_NAME -y
-conda create -n $ENV_NAME python=3.8 -y
+conda create -n $ENV_NAME python=3.10 -y
 conda activate $ENV_NAME
 
-conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch -y
+conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 pytorch-cuda=11.8 -c pytorch -c nvidia -y
 conda install -c conda-forge tensorboard -y
 conda install mkl=2024.0.0 -y # needs to downgraded because of some bug in other version
 
-
-############## 2) get further required packages that have been confirmed to work together on our system
-pip install jupyter
-pip install torchinfo
-pip install openmim==0.3.9
-mim install mmengine==0.10.3
-mim install mmcv==2.0.1
-
-####### alternatively, run these commands to get a newer version of the same packages (has not been tested and might require debugging)
-# pip install jupyter
-# pip install -U openmim
-# mim install mmengine
-# mim install "mmcv>=2.0.0"
-
+############## 2) get further required packages
+pip install -U openmim
+mim install mmengine
+mim install "mmcv>=2.0.0"
 
 ############## 3) initialize submodules to the commits specified in this repo
 ############## mmdetection commit hash: cfd5d3a985b0249de009b67d04f37263e11cdf3d
@@ -41,7 +31,6 @@ git submodule update --init --recursive
 # git pull
 # cd ..
 
-
 ############## 4) build mmdetection, mmyolo and detection_utils
 cd mmdetection
 pip install -v -e .
@@ -49,9 +38,12 @@ cd ..
 
 cd mmyolo
 pip install -r requirements/albu.txt
+pip install albumentations==1.3.1 # this downgrade is necessary to avoid conflicts with mmyolo
 mim install -v -e .
 cd ..
 
 pip install -v -e .
 
+# other conda packages
+conda install jupyter -y
 conda deactivate
